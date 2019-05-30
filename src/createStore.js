fd5757876e52984f568b6657b35992071fd38330
Ge Guo <alex@Ges-MacBook-Pro.local>
2019-05-30 18:05:24 +1000

    Add redux to my app

---
+++ src/createStore.js
@@ -0,0 +1,11 @@
+import { createStore, applyMiddleware } from "redux";
+import RootReducers from "./reducers";
+import ReduxThunk from "redux-thunk";
+
+export const middlewares = [ReduxThunk];
+
+export const createStoreWithMiddleware = applyMiddleware(...middlewares)(
+  createStore
+);
+
+export const store = createStoreWithMiddleware(RootReducers);
