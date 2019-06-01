e19b3456a70d8e116a0bdea117988e4ba230e1ed
Ge Guo <alex@Ges-MacBook-Pro.local>
2019-06-02 00:46:55 +1000

    added mothod test in app.js

--- src/App.js
+++ src/App.js
@@ -17,18 +17,39 @@ const tempArr = [
   }
 ];
 
+const initialState = {
+  hideBtn: false
+};
+
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      ...initialState
+    };
     this.fetch = this.fetch.bind(this);
   }
 
   fetch = () => {
     this.props.fetchPosts();
+    this.exampleMethod_updatesState();
   };
+
+  exampleMethod_updatesState() {
+    const { hideBtn } = this.state;
+    this.setState({
+      hideBtn: !hideBtn
+    });
+  }
+
+  exampleMethod_returnsAValue(number) {
+    return number + 1;
+  }
 
   render() {
     const { posts } = this.props;
+    const { hideBtn } = this.state;
+
     const configButton = {
       buttonText: "Get Posts",
       emitEvent: this.fetch
@@ -42,7 +63,7 @@ class App extends React.Component {
             desc="Click the button to render posts"
             tempArr={tempArr}
           />
-          <SharedButton {...configButton} />
+          {!hideBtn && <SharedButton {...configButton} />}
           {posts.length > 0 && (
             <div>
               {posts.map((post, index) => {
